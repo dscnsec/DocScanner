ff0eb3ee87b2f1b57dd922d3b3ebcfad84cd4e40
Kartikey <rawatkari554@gmail.com>
2020-12-17 23:40:26 +0530

    Done with align_images.py file

--- align_document.py
+++ align_document.py
@@ -1,1 +1,36 @@
+from dscnsec.alignment import align_images
+import numpy as np
+import argparse
+import imutils
+import cv2
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image that we'll align to template")
+ap.add_argument("-t", "--template", required=True,
+	help="path to input template image")
+args = vars(ap.parse_args())
 
+# load the input image and template from disk
+print("[INFO] loading images...")
+image = cv2.imread(args["image"])
+template = cv2.imread(args["template"])
+# align the images
+print("[INFO] aligning images...")
+aligned = align_images(image, template, debug=True)
+
+# visualize them on our screen
+aligned = imutils.resize(aligned, width=700)
+template = imutils.resize(template, width=700)
+# our first output visualization of the image alignment will be a
+# side-by-side comparison of the output aligned image and the
+# template
+stacked = np.hstack([aligned, template])
+
+overlay = template.copy()
+output = aligned.copy()
+cv2.addWeighted(overlay, 0.5, output, 0.5, 0, output)
+# show the two output image alignment visualizations
+cv2.imshow("Image Alignment Stacked", stacked)
+cv2.imshow("Image Alignment Overlay", output)
+cv2.waitKey(0)
